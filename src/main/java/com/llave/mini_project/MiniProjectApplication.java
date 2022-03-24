6353af5591a2cad9ac91ce62fed5641fd42f0672
Baek Seungho <seungho1216@naver.com>
2022-03-24 16:52:40 +0900

    xss방지를 위한 lucy-xss-servlet-filter 적용 JSON.stringify()로 인한 문제 해결

--- src/main/java/com/llave/mini_project/MiniProjectApplication.java
+++ src/main/java/com/llave/mini_project/MiniProjectApplication.java
@@ -1,7 +1,10 @@
 package com.llave.mini_project;
 
+import com.navercorp.lucy.security.xss.servletfilter.XssEscapeServletFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import javax.annotation.PostConstruct;
@@ -21,6 +24,15 @@ public class MiniProjectApplication {
         TimeZone.setDefault(tz);
         System.out.println("현재시각:" + new Date());
         System.out.println(ZonedDateTime.now(ZoneId.of("Asia/Seoul")));
+    }
+
+    @Bean
+    public FilterRegistrationBean xssEscapeServletFilter() {
+        FilterRegistrationBean registrationBean = new FilterRegistrationBean();
+        registrationBean.setFilter(new XssEscapeServletFilter());
+        registrationBean.setOrder(1);  // @Order로 처리.
+        registrationBean.addUrlPatterns("/*");
+        return registrationBean;
     }
 
     public static void main(String[] args) {
